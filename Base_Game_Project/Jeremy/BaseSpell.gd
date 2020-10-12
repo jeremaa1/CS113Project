f2a38d7a0e326fb0140c2b46bf8c697fa07e4c9d
jeremaa1 <62965830+jeremaa1@users.noreply.github.com>
2020-10-11 19:05:38 -0700

    Projectile Prototype
    Created a basic spell object and added to the player script (Player_V2.gd) to allow the player to shoot magic shots.
    Test by running the scene worldProjectile.tscn found in the Jeremy folder

---
+++ Base_Game_Project/Jeremy/BaseSpell.gd
@@ -0,0 +1,32 @@
+extends Area2D
+
+const SPEED = 500
+var velocity = Vector2()
+var direction = 1
+#1 is right, -1 is left
+
+func _ready():
+	pass # Replace with function body.
+
+func set_direction(dir):
+	direction = dir
+	if dir == -1:
+		$AnimatedSprite.flip_h = true
+
+func _process(delta):
+	velocity.x = SPEED * delta * direction
+	translate(velocity)
+	$AnimatedSprite.play("shoot")
+
+
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()	#Deletes the object when it moves offscreen
+	"""
+	Look into queue_free()
+	consider using a temp. timer to count 2 secs
+	and then delete the spell
+	"""
+
+#Function makes spell destroy self on any form of collision
+func _on_BaseSpell_body_entered(body):
+	queue_free()
---
+++ Base_Game_Project/Jeremy/Player_V2.gd
@@ -0,0 +1,50 @@
+#Player_V2
+
+#This script adds the ability to shoot magic
+#Hopefully
+
+extends KinematicBody2D
+
+const UP = Vector2(0, -1)
+const GRAVITY = 20
+const SPEED = 200
+const JUMP_HEIGHT = -550
+var motion = Vector2()
+
+#Loads the BaseSpell object for optimization
+const SPELL = preload("res://Jeremy/BaseSpell.tscn")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	motion.y += GRAVITY
+	
+	#Added a sign check on the 2d position,
+	#moves it left and right with the character
+	if Input.is_action_pressed("ui_right"):
+		motion.x = SPEED
+		if sign($Position2D.position.x) == -1:
+			$Position2D.position.x *= -1
+		
+	elif Input.is_action_pressed("ui_left"):
+		motion.x = -SPEED
+		if sign($Position2D.position.x) == 1:
+			$Position2D.position.x *= -1
+			
+	else:
+		motion.x = 0
+	
+	#Spell currently set to TAB
+	if Input.is_action_just_pressed("ui_focus_next"):
+		var spell = SPELL.instance()
+		spell.set_direction(sign($Position2D.position.x))
+		get_parent().add_child(spell)
+		spell.position = $Position2D.global_position
+	#End of spawning a spell instance
+	
+	if is_on_floor():
+		if Input.is_action_just_pressed("ui_up"):
+			motion.y = JUMP_HEIGHT
+	motion = move_and_slide(motion, UP)
+	print(motion)
+	pass
