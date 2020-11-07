71596fce3ad7ce76a25cc3c1099502f4c0837898
jeremaa1 <62965830+jeremaa1@users.noreply.github.com>
2020-11-06 16:04:48 -0800

    Gave damage to all spells
    Each spell currently deals a set amount of damage
    Base => 25
    Ice => 25
    Fire => 50
    Elec => 10

--- Base_Game_Project/Jeremy/BaseSpell.gd
+++ Base_Game_Project/Jeremy/BaseSpell.gd
@@ -31,6 +31,6 @@ func _on_VisibilityNotifier2D_screen_exited():
 func _on_BaseSpell_body_entered(body):
 	#ADDED
 	if "Enemy" in body.name:
-		body.take_damage(50)
+		body.take_damage(25)
 	#END 
 	queue_free()
